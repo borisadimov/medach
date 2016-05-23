06899824e7a1343a20f4b794eafa70c9abb776d8
Boris Adimov <boris@adimov.ru>
2016-05-23 17:03:33 +0300

    finished basic config

---
+++ packages/custom-components/lib/common/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = props => {
+  return (
+    <div className="footer" style={{fontSize: '0.7rem'}}>
+      {'Created by '}
+      <a href="http://adimov.ru" target="_blank">
+        Boris Adimov
+      </a>
+      {' using '}
+      <a href="http://telescopeapp.org" target="_blank">
+        Telescope
+      </a>
+    </div>
+  )
+}
+
+module.exports = Footer;
